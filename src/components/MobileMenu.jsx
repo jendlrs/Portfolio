1e2a8c72cf02ca5461583463d58d874290e82bfd
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2025-02-14 18:21:48 +0800

    feat: add onClick handler to X button for closing

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -12,7 +12,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
     }
     `}>
 
-        <button className = "absolute top-6 right-6 text-white text-3xl focus:outline-none-cursor-pointer">
+        <button onClick={() => setMenuOpen(false)} className = "absolute top-6 right-6 text-white text-3xl focus:outline-none-cursor-pointer">
             &times; 
 
         </button>
